d44fb660eeb8d3c87278366335ab91f093ad3f1f
Ricardo2dds <144152488+Ricardo2dds@users.noreply.github.com>
2024-05-31 23:40:03 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // Importa el componente App desde App.js
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Renderiza el componente App */}
   </React.StrictMode>,
   document.getElementById('root')
 );
